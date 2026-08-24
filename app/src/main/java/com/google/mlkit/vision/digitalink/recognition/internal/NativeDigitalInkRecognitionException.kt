fd852622689647ea8cc8f30ce9bc33a133c36e2d
LeanBitLab <Leanbitlab@gmail.com>
2026-08-24 20:41:05 +0530

    fix(handwriting): declare NativeDigitalInkRecognitionException to bridge C++ exceptions

---
+++ app/src/main/java/com/google/mlkit/vision/digitalink/recognition/internal/NativeDigitalInkRecognitionException.kt
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+package com.google.mlkit.vision.digitalink.recognition.internal
+
+import androidx.annotation.Keep
+
+@Keep
+class NativeDigitalInkRecognitionException(message: String) : Exception(message)
